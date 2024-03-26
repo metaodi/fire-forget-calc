5460ac62e474accdfbe450506b0184307d749bfb
Stefan Oderbolz <oderbolz@gmail.com>
2024-03-26 10:11:38 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -18,7 +18,9 @@ from docopt import docopt
 import json
 from pprint import pprint
 from datetime import datetime
+import pytz
 import uuid
+import time
 
 
 arguments = docopt(__doc__, version="Calculate something 1.0")
@@ -26,11 +28,17 @@ arguments = docopt(__doc__, version="Calculate something 1.0")
 input = arguments['--input']
 output = arguments['--output']
 
+tz = pytz.timezone('Europe/Zurich')
+zurich_now = datetime.now(tz)
+
 result = {
   "hallo": "velo",
   "input": f"{input} {str(uuid.uuid4())}",
-  "update_datetime": datetime.now().isoformat(),
+  "update_datetime": zurich_now.isoformat(),
 }
+
+# pretend to do something time consuming
+time.sleep(60)
 
 print("Result:")
 pprint(result)
