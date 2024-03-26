4fa24790b2a684841b209ccda3c88ba85c9c744d
Stefan Oderbolz <oderbolz@gmail.com>
2024-03-26 10:16:49 +0100

    Add ruff extension

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@
 """Calculate something based on the input and save to output file.
 
 Usage:
-  calc.py --input <input> --output <output> 
+  calc.py --input <input> --output <output>
   calc.py (-h | --help)
   calc.py --version
 
@@ -12,7 +12,6 @@ Options:
   -i, --input <input>     Input for calculation.
   -o, --output <output>   Path to output JSON.
 """
-
 
 from docopt import docopt
 import json
@@ -25,16 +24,16 @@ import time
 
 arguments = docopt(__doc__, version="Calculate something 1.0")
 
-input = arguments['--input']
-output = arguments['--output']
+input = arguments["--input"]
+output = arguments["--output"]
 
-tz = pytz.timezone('Europe/Zurich')
+tz = pytz.timezone("Europe/Zurich")
 zurich_now = datetime.now(tz)
 
 result = {
-  "hallo": "velo",
-  "input": f"{input} {str(uuid.uuid4())}",
-  "update_datetime": zurich_now.isoformat(),
+    "hallo": "velo",
+    "input": f"{input} {str(uuid.uuid4())}",
+    "update_datetime": zurich_now.isoformat(),
 }
 
 # pretend to do something time consuming
@@ -44,4 +43,4 @@ print("Result:")
 pprint(result)
 
 with open(output, "w") as f:
-  f.write(json.dumps(result, indent=4))
+    f.write(json.dumps(result, indent=4))
